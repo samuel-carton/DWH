81ff2af513d3ac8662f70354ea1cd4ca299a760f
scartoning2020 <35373637+scartoning2020@users.noreply.github.com>
2018-06-02 15:40:04 +0200

    un peut de extract

--- ETLCodeSkeleton.sql
+++ ETLCodeSkeleton.sql
@@ -11,6 +11,22 @@
 -- --------------------------------------------
 -- pre-extract report
 -- --------------------------------------------
+drop table Member_Extract;
+create table Member_Extract(
+  memberno NUMBER(6,0),
+  name VARCHAR2(50 BYTE),
+  zipcode NUMBER(4,0),
+  dateborn date,
+  datejoined date,
+  dateleft date,
+  student CHAR(1 BYTE),
+  pilot CHAR(1 BYTE),
+  cat CHAR(1 BYTE),
+  fullcat CHAR(1 BYTE),
+  typeOfChange number
+);
+
+
 
 select 'Extract Members at: ' || to_char(sysdate,'YYYY-MM-DD HH24:MI') as extractTime
   from dual
@@ -20,18 +36,36 @@ select 'Extract Members at: ' || to_char(sysdate,'YYYY-MM-DD HH24:MI') as extrac
     from dual
 union all
   select 'Rows in extract table before (should be zero)' || to_char(count(*))
-   from ETLMembersExtract
+   from Member_Extract
 union all
   select 'Rows in Members table ' || to_char(count(*))
    from taMember
 union all
   select 'Rows in Members table (yesterday copy) ' || to_char(count(*))
-   from taMemberYesterday
+   from TaMemberYesterday
  ;
 
 -- **** here goes the extract of added rows 
 -- **** (i.e. rows from today whose primary key is in the set of (PKs from today minus PKs yesterday)
-		select *
+insert into Member_Extract(
+  MEMBERNO , 
+	INITIALS , 
+	NAME , 
+	ADDRESS , 
+	ZIPCODE  , 
+	DATEBORN , 
+	DATEJOINED , 
+	DATELEFT , 
+	OWNSPLANEREG , 
+	STATUSSTUDENT , 
+	STATUSPILOT  , 
+	STATUSASCAT , 
+	STATUSFULLCAT, 
+	SEX , 
+	CLUB,
+  typeOfChange)
+    (
+    select *,1
 			from taMember
 			where id in
 			(
@@ -40,13 +74,30 @@ union all
 			minus
 				select ID
 					from taMemberYesterday
-			);
+			));
 
 
 -- **** here goes the extract of deleted rows 
 -- **** (i.e. rows from yesterday whose primary key is in the set of (PKs from yesterday minus PKs today)
 
-		select *
+		insert into Member_Extract(
+  MEMBERNO , 
+	INITIALS , 
+	NAME , 
+	ADDRESS , 
+	ZIPCODE  , 
+	DATEBORN , 
+	DATEJOINED , 
+	DATELEFT , 
+	OWNSPLANEREG , 
+	STATUSSTUDENT , 
+	STATUSPILOT  , 
+	STATUSASCAT , 
+	STATUSFULLCAT, 
+	SEX , 
+	CLUB,
+  typeOfChange)
+    (select *,2
 			from taMember
 			where id in
 			(
@@ -55,12 +106,29 @@ union all
 			minus
 				select ID
 					from taMember
-			);
+			));
 
 -- **** here goes the extract of changed rows 
 -- **** (i.e. (rows from today minus rows from yesterday) - new rows )
-	
-		select *
+	insert into Member_Extract(
+  MEMBERNO , 
+	INITIALS , 
+	NAME , 
+	ADDRESS , 
+	ZIPCODE  , 
+	DATEBORN , 
+	DATEJOINED , 
+	DATELEFT , 
+	OWNSPLANEREG , 
+	STATUSSTUDENT , 
+	STATUSPILOT  , 
+	STATUSASCAT , 
+	STATUSFULLCAT, 
+	SEX , 
+	CLUB,
+  typeOfChange)
+    (
+		select *,3
 			from (
 				select * from taMember
 			minus
@@ -71,33 +139,33 @@ union all
 				select ID from taMember
 			minus
 				select ID from taMemberYesterday
-		);
+		));
 
 	
 		
 -- **** here goes the extract of Members whose age changed (i.e. those who have a birthday today) 
 -- **** (i.e. (rows from today with dateBorn(DDMM) = current date(DDMM) - already extracted PKs)
-        
-    select* 
-        from (select *
-			from (
-				select * from taMember
-			minus
-				select * from taMemberYesterday
-			) changes
-		where not changes.id in
-		(
-				select ID from taMember
-			minus
-				select ID from taMemberYesterday
-		); )
-        
-        where taMember.dateborn = CURDATE() ;
-
-
-
-
-
+     insert into Member_Extract(
+  MEMBERNO , 
+	INITIALS , 
+	NAME , 
+	ADDRESS , 
+	ZIPCODE  , 
+	DATEBORN , 
+	DATEJOINED , 
+	DATELEFT , 
+	OWNSPLANEREG , 
+	STATUSSTUDENT , 
+	STATUSPILOT  , 
+	STATUSASCAT , 
+	STATUSFULLCAT, 
+	SEX , 
+	CLUB,
+  typeOfChange)
+    (   
+    select*,4 
+        from taMember    
+    where taMember.dateborn = CURDATE()) ;
 
 
 commit
@@ -114,6 +182,43 @@ select operation
 from ETLMembersExtract
  group by operation
  ;
+ 
+ drop table TaMemberYesterday;
+ create table TaMemberYesterday(
+  "MEMBERNO" NUMBER(6,0) , 
+	"INITIALS" CHAR(4 BYTE) , 
+	"NAME" VARCHAR2(50 BYTE) , 
+	"ADDRESS" VARCHAR2(50 BYTE) , 
+	"ZIPCODE" NUMBER(4,0) , 
+	"DATEBORN" DATE , 
+	"DATEJOINED" DATE , 
+	"DATELEFT" DATE, 
+	"OWNSPLANEREG" CHAR(3 BYTE) , 
+	"STATUSSTUDENT" CHAR(1 BYTE) , 
+	"STATUSPILOT" CHAR(1 BYTE) , 
+	"STATUSASCAT" CHAR(1 BYTE) , 
+	"STATUSFULLCAT" CHAR(1 BYTE) , 
+	"SEX" CHAR(1 BYTE) , 
+	"CLUB" VARCHAR2(50 BYTE) 
+);
+ insert into TaMemberYesterday(
+ MEMBERNO , 
+	INITIALS , 
+	NAME , 
+	ADDRESS , 
+	ZIPCODE  , 
+	DATEBORN , 
+	DATEJOINED , 
+	DATELEFT , 
+	OWNSPLANEREG , 
+	STATUSSTUDENT , 
+	STATUSPILOT  , 
+	STATUSASCAT , 
+	STATUSFULLCAT, 
+	SEX , 
+	CLUB)
+ (select * from tamember);
+ 
  
 exit;
 
